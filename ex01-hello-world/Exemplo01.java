fb969911d69a446e0c5d930e1feee7d8d42ec928
Luanna Silva <luannaevellyn2020@gmail.com>
2025-05-19 14:07:37 +0000

    hello world + exemplo de funções

---
+++ ex01-hello-world/Exemplo01.java
@@ -0,0 +1,14 @@
+public class Exemplo01{
+    public static void main (String args[]){
+    //System.out.print("Olá mundo!");
+    System.out.println("o dobro de 10.7 é " + dobro(10.7));
+    }  
+
+    public static int dobro(int num){
+        return 2 * num;
+    }
+    
+    public static double dobro(double num){
+        return 2 * num;
+    }
+}
